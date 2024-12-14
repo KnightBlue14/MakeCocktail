27a0050c7e3fe600ef93eca62d1992dfb5de9e8f
KnightBlue <domburton@hotmail.co.uk>
2024-12-14 18:11:30 +0000

    pre-pull

--- test_MakeCocktails.py
+++ test_MakeCocktails.py
@@ -7,17 +7,18 @@ class TestIngredient(unittest.TestCase):
         Ingred1 = Ingredient(5,2,50)
         self.assertEqual(Ingred1.shots, 2,'There are two standard shots')
         self.assertGreater(Ingred1.cost,Ingred1.sum,'The cost is greater than the total revenue')
-    def test_exceptions(self):
-        try:
-            Ingred2 = Ingredient(10,5,0)
-        except ZeroDivisionError as exc:
-            self.assertRaises(ZeroDivisionError, 'If the volume is zero, it should result in division errors')
+    def test_exceptions_zero(self):
+      with self.assertRaises(ZeroDivisionError): 
+         Ingred2 = Ingredient(10,5,0)
     def test_spirit(self):
         Spirit1 = Spirit(10,2.5,700,37.5)
         Spirit2 = Spirit(20,3,700,40)
         self.assertEqual(Spirit1.proof, 75, 'The proof is double the abv')
         self.assertLess(Spirit1.unit_per, 1,'It would be 1 if the abv was 40')
         self.assertLess(Spirit1.unit_per,Spirit2.unit_per, 'Spirit1 has a lower abv')
+    def test_NonAlc(self):
+        SpiritNon1 = Spirit(5,5,700,0)  
+        self.assertTrue(SpiritNon1.IsNonAlc)
     def test_mix(self):
         Mix1 = Mix(2,1.5,1000,250)
 
